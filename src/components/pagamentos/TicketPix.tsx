b2ab0057b71934d7c6319280d1adc0c8d23e52a8
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-12-11 15:27:34 -0300

    configuring front

---
+++ src/components/pagamentos/TicketPix.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+const TicketPix = () => {
+  return (
+    <div>
+      <div>TicketPix</div>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default TicketPix;
